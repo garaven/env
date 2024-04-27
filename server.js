5279a4ce00ea9258433d2618ea0a0470cc82c18d
unknown <oscarandreshernandezpineda@gmail.com>
2024-04-27 16:24:52 -0500

    User registering on database

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require("path");
+const bodyParser = require("body-parser")
+const db = require('./database/connection-db');
 
 app.listen(3000, ()=>{
   console.log("Server running")
 })
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) =>{
   res.sendFile(path.join(__dirname + "/view/src/index.html"));
+})
+
+app.post("/index", (req, res) =>{
+  const { name, email, password, acc_type, country } = req.body;
+  db.registerUser(name, email, password, acc_type, country, (err, result) =>{
+    if (err) {
+      return res.status(500).send('Error registering user');
+    }
+    res.send('User registered sucesfully');
+  })
 })
