7050eda47668c3802016b59f19e8ff281ade057c
unknown <oscarandreshernandezpineda@gmail.com>
2024-04-28 20:04:36 -0500

    Add device to database

--- database/connection-db.js
+++ database/connection-db.js
@@ -46,7 +46,19 @@ function loginUser(email, password, callback) {
   });
 }
 
+function addDevice(name, brand, consumption, usage_time, callback) {
+  const sql = `INSERT INTO device(name, brand, consumption, usage_time, date) VALUES (?, ?, ?, ?, NOW())`;
+  db.query(sql, [name, brand, consumption, usage_time], (err, result) => {
+    if (err) {
+      console.error('Error on database:', err);
+      return callback(err);
+    }
+    callback(null, result);
+    });
+}
+
 module.exports = {
   registerUser,
-  loginUser
+  loginUser,
+  addDevice
 };
