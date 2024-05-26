326e28f898736a640530bdad0ccc4dfdacdea4df
unknown <oscarandreshernandezpineda@gmail.com>
2024-05-25 23:29:26 -0500

    Doc on server.js / Styles on edit device

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
+// External Modules.
 const express = require('express');
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
 
+// Express server configuration.
 app.listen(3000, () => {
   console.log("Server running")
-})
+});
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static('view'));
+//Middlewares.
+app.use(bodyParser.urlencoded({ extended: true })); // To analize URL body requests.
+app.use(bodyParser.json()); // To analize JSON requests.
+app.use(express.static('view')); // To use static files from view folder.
 
+// Main Routers.
 const staticRouter = require('./routes/staticRoutes')
 app.use(staticRouter);
 
